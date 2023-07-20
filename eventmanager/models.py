a6eb4e90c60aaa787116ed553634f44384ed0c39
shahwaiz14 <shahwaiz14@hotmail.com>
2023-07-20 09:10:39 -0500

    Add events tables

--- eventmanager/models.py
+++ eventmanager/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+   
+
+class EventLog(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    data = models.JSONField()
